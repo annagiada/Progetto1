d9de7dd2aec5ad8acf2209031758704ae4b2bd4c
Giamm0101 <giammarcomuscedere98@gmail.com>
2024-05-22 11:26:50 +0200

    pagina artisti

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import mysql.connector
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+
+def create_db_connection():
+    db_config = {
+        'host': app.config['MYSQL_HOST'],
+        'user': app.config['MYSQL_USER'],
+        'password': app.config['MYSQL_PASSWORD'],
+        'database': app.config['MYSQL_DB']
+    }
+    return mysql.connector.connect(**db_config)
+
+def execute_query(query, params=None):
+    connection = create_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
+
+#___________________________________________________________________________________________________________
+# Lista di esempio di 1323 artisti
+artists =  [execute_query('SELECT * FROM artist')]
+artists_per_page = 20
 
 
-@app.route('/home')
+#___________________________________________________________________________
+@app.route('/')
 def home():
     
     return render_template('home.html')
+
+
+@app.route('/artisti')
+def artisti():
+    artisti = execute_query('SELECT * FROM artist')
+    return render_template('artisti.html', artisti=artisti)
+
+
+@app.route('/opere')
+def opere():
+    return render_template('opere.html')
 
 
 if __name__ == '__main__':
